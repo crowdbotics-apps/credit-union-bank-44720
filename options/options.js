13e3571f3ecee1cdb62ed4cfe68a0b568f99b450
Crowdbotics <team@crowdbotics.com>
2023-11-23 01:20:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "credit-union-bank-44720",
+  url: "https://credit-union-bank-44720.botics.co",
+  api: "https://credit-union-bank-44720.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
